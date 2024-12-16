8b2afec59c101b4d2813740c0ea988b98eb02ba0
MylleAmorim <111822365+MylleAmorim@users.noreply.github.com>
2024-12-16 19:48:01 -0300

    Delete src directory

--- src/main/java/com/alugueagora/dtos/RentsDtos.java
+++
@@ -1,9 +0,0 @@
-package com.alugueagora.dtos;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.security.Timestamp;
-
-public record RentsDtos(@NotBlank String description, @NotBlank String summary , @NotNull Double price, @NotNull Double condominium, @NotNull Double fees, @NotNull Timestamp createAt) {
-
-}
